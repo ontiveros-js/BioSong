8b6be00b68294410a25c6c971678573c8617ccb3
douglas ontiveros <douglasontiveros1408@gmail.com>
2022-01-21 16:31:44 -0300

    Updates

---
+++ precache-manifest.b65d3c552885c72357eacb236fcd27c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6193fede8c64d195b43d180a70bc4784",
+    "url": "/BioSong/index.html"
+  },
+  {
+    "revision": "29b2cacc4f59f109df6b",
+    "url": "/BioSong/static/css/main.592668bd.chunk.css"
+  },
+  {
+    "revision": "9b6932fc7f6a56f5269a",
+    "url": "/BioSong/static/js/2.d7d91a99.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/BioSong/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "29b2cacc4f59f109df6b",
+    "url": "/BioSong/static/js/main.59d330bc.chunk.js"
+  },
+  {
+    "revision": "ca41b9a9b852b8a3ffd3",
+    "url": "/BioSong/static/js/runtime~main.e47a5e9a.js"
+  }
+]);
